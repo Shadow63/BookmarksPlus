c14dbdb2dc8f167e19ce59480d49c1781333e5af
Finn Feighery <peterlee26717@yahoo.com>
2019-10-12 15:50:47 -0400

    Test1

---
+++ CreateBookspace.py
@@ -0,0 +1,22 @@
+from PyQt5.QtWidgets import *
+
+app = QApplication([])
+
+
+class CreateBookspace:
+
+    window = QWidget()
+
+
+    layout = QVBoxLayout()
+    fname = QLabel()
+    name = QLineEdit()
+
+    #layout.addWidget(fname)
+    layout.addWidget(name)
+    layout.addWidget(QPushButton('Top'))
+    layout.addWidget(QPushButton('Bottom'))
+    window.setLayout(layout)
+    window.show()
+
+app.exec_()
