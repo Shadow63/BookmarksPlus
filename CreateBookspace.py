8f0885ab1002428a0ed4c2e1222bde7864cbba3e
Finn Feighery <peterlee26717@yahoo.com>
2019-10-12 16:04:36 -0400

    Placed the run maethod inside class

--- CreateBookspace.py
+++ CreateBookspace.py
@@ -4,20 +4,24 @@ app = QApplication([])
 
 
 class CreateBookspace:
-
+    # Main Window
     window = QWidget()
-
-
     layout = QVBoxLayout()
+
+    # Setting the name of the Bookspace
     fname = QLabel()
     name = QLineEdit()
 
-    #layout.addWidget(fname)
+    layout.addWidget(fname)
     layout.addWidget(name)
+
+    # Some Buttons
     layout.addWidget(QPushButton('Top'))
     layout.addWidget(QPushButton('Bottom'))
     window.setLayout(layout)
     window.show()
 
-def run():
-    app.exec_()
+    def run():
+        app.exec_()
+
+    run()
