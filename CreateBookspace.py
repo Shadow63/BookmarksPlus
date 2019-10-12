53583a36d83763b2279971bc3104ca386aa85ffe
Finn Feighery <peterlee26717@yahoo.com>
2019-10-12 17:22:33 -0400

    Hope it works, gl Anh

--- CreateBookspace.py
+++ CreateBookspace.py
@@ -1,27 +1,53 @@
+import sys
+from PyQt5 import QtGui
 from PyQt5.QtWidgets import *
 
-app = QApplication([])
+#app = QApplication([])
 
+def get_name(something):
+    print(something.text())
 
-class CreateBookspace:
+class CreateBookspace(QWidget):
     # Main Window
-    window = QWidget()
-    layout = QVBoxLayout()
+    def __init__(self):
+        super().__init__()
+        self.initui()
 
-    # Setting the name of the Bookspace
-    fname = QLabel()
-    name = QLineEdit()
+    def initui(self):
+        layout = QVBoxLayout()
+        layout2 = QHBoxLayout()
 
-    layout.addWidget(fname)
-    layout.addWidget(name)
+        # Setting the name of the Bookspace
+        fname = QLabel()
+        fname.setText("Name of this Bookspace: ")
+        name = QLineEdit()
 
-    # Some Buttons
-    layout.addWidget(QPushButton('Top'))
-    layout.addWidget(QPushButton('Bottom'))
-    window.setLayout(layout)
-    window.show()
+        layout2.addWidget(fname)
+        layout2.addWidget(name)
 
-    def run():
-        app.exec_()
 
-    run()
+        # Some Buttons
+        layout.addLayout(layout2)
+        create = QPushButton('Create')
+        word = name.text()
+
+        layout.addWidget(create)
+        create.clicked.connect(lambda: self.handleButton())
+        layout.addWidget(QPushButton('Bottom'))
+        self.setLayout(layout)
+
+    def handleButton(self):
+        print(self.get_name)
+
+    def get_name(self):
+        print(self.name.text)
+        return self.name.text()
+
+
+def run():
+    app = QApplication(sys.argv)
+    ex = CreateBookspace()
+    ex.show()
+    sys.exit(app.exec_())
+
+run()
