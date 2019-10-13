fac7ba93f76a36578249b1fae2a251b62028238c
Anh Nguyen <anhog9@yahoo.com>
2019-10-13 03:42:38 -0400

    Adds fancy functionalities.

--- MainWindow.py
+++ MainWindow.py
@@ -132,7 +132,7 @@ class Ui_MainWindow(object):
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:18pt; color:#ffaa00;\">SELECT A SCRIPT!</span></p></body></html>"))
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:18pt; color:#ffaa00;\">SELECT A BOOKSPACE!</span></p></body></html>"))
         self.menuFILE.setTitle(_translate("MainWindow", "File"))
         self.actionNew_Bookspace.setText(_translate("MainWindow", "New Bookspace"))
         self.actionNew_Bookspace.setStatusTip(_translate("MainWindow", "Create a new bookspace"))
