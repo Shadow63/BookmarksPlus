22891be4abc409f81952e6dd14b4cf1db7932dfd
Anh Nguyen <anhog9@yahoo.com>
2019-10-12 23:16:48 -0400

    Updated Bookspace backend.

--- Bookspace.py
+++ Bookspace.py
@@ -22,26 +22,61 @@ class BookspacesData:
 
     # Adds a bookspace to the database
     @staticmethod
-    def add_bookspace(bookspace_name, urls):
+    def add_bookspace(bookspace_name, urls, usernames, passwords, is_focused, has_next):
         bookspaces_list = BookspacesData.get_bookspaces()
 
         # Checks if name is a duplicate
         if bookspace_name not in bookspaces_list:
             with open(BookspacesData.bookspaces_data_file, 'a') as f:
                 f.write(bookspace_name + '\n')
-
             file_ptr = open('scripts/' + bookspace_name + '.py', 'a')
-            file_ptr.write(
+
+            # Creating script file, only if there are links
+            if len(urls) > 0:
+                file_ptr.write(
 """from selenium import webdriver
+import pyautogui
+import time
 
 browser = webdriver.Firefox()
-browser.get('""" + urls[0] + "')\n\n")
+browser.get('""" + urls[0] + "')\n")
 
-            for i in range(1, len(urls)):
-                open_url_script = 'window.open("' + urls[i] + '", "_blank")'
+                # Handling logins, only if there are more than 1 login credentials
+                if len(usernames[0]) > 0 and not is_focused[0] and not has_next[0]:
+                    file_ptr.write(
+"""time.sleep(10)
+pyautogui.press('tab')
+time.sleep(1)
+pyautogui.typewrite('""" + usernames[0] +
+"')\n" + """time.sleep(1)\npyautogui.press('tab')\npyautogui.typewrite('""" + passwords[0] + "')\n" +
+"pyautogui.press('enter')\n\n")
+                if len(usernames[0]) > 0 and is_focused[0] and not has_next[0]:
+                    file_ptr.write(
+"""time.sleep(10)
+pyautogui.typewrite('""" + usernames[0] +
+"')\n" + """time.sleep(1)\npyautogui.press('tab')\npyautogui.typewrite('""" + passwords[0] + "')\n" +
+"pyautogui.press('enter')\n\n")
+#                 if len(usernames[0]) > 0 and is_focused[0] and has_next[0]:
+#                     file_ptr.write(
+# """time.sleep(10)
+# pyautogui.typewrite(""" + usernames[0] +
+# ")\n" + """time.sleep(1)\npyautogui.press('tab')\npyautogui.typewrite(""" + passwords[
+#     0] + ")\n\n")
 
-                # Write to script file
-                file_ptr.write("browser.execute_script('" + open_url_script + "')\n")
+                for i in range(1, len(urls)):
+                    open_url_script = 'window.open("' + urls[i] + '", "_blank")'
+
+                    # Write to script file
+                    file_ptr.write("browser.execute_script('" + open_url_script + "')\n")
+
+                    if len(usernames[i]) > 0 and not is_focused[i] and not has_next[i]:
+                        file_ptr.write(
+"""time.sleep(10)
+pyautogui.press('tab')
+time.sleep(1)
+pyautogui.typewrite(""" + usernames[i] +
+")\n" + """time.sleep(1)\npyautogui.press('tab')\npyautogui.typewrite(""" + passwords[i] + ")\n" +
+"pyautogui.press('enter')\n\n")
         else:
             print('error: bookspace name already exists')
 
